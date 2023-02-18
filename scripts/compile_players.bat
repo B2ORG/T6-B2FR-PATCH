2b998473c9a84a4eaa293168f2af38623ee25f3a
Zi0MIX <80110206+Zi0MIX@users.noreply.github.com>
2023-02-18 15:57:17 +0100

    Removed compile scripts from .gitignore

---
+++ scripts/compile_players.bat
@@ -0,0 +1,8 @@
+gsc-tool.exe comp t6 "Player Raws/plant.gsc"
+gsc-tool.exe comp t6 "Player Raws/zi0.gsc"
+gsc-tool.exe comp t6 "Player Raws/tonestone.gsc"
+gsc-tool.exe comp t6 "Player Raws/tek.gsc"
+gsc-tool.exe comp t6 "Player Raws/shadez.gsc"
+gsc-tool.exe comp t6 "Player Raws/yojurt.gsc"
+gsc-tool.exe comp t6 "Player Raws/vistek.gsc"
+pause
---
+++ scripts/compile_plugins.bat
@@ -0,0 +1,5 @@
+gsc-tool.exe comp t6 Plugins/frfix_hud_override.gsc
+gsc-tool.exe comp t6 Plugins/frfix_metal_boards.gsc
+gsc-tool.exe comp t6 Plugins/frfix_testing.gsc
+gsc-tool.exe comp t6 Plugins/frfix_debug_permaperks.gsc
+pause
