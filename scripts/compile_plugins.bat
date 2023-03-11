93146052c05006e8346152655f669dd9a5cdd51a
Zi0MIX <80110206+Zi0MIX@users.noreply.github.com>
2023-03-11 10:38:20 +0100

    Added zone display plugin

--- scripts/compile_plugins.bat
+++ scripts/compile_plugins.bat
@@ -2,4 +2,6 @@ gsc-tool.exe comp t6 Plugins/frfix_hud_override.gsc
 gsc-tool.exe comp t6 Plugins/frfix_metal_boards.gsc
 gsc-tool.exe comp t6 Plugins/frfix_testing.gsc
 gsc-tool.exe comp t6 Plugins/frfix_debug_permaperks.gsc
+
+gsc-tool.exe comp t6 Plugins/frfix_plugin_zones.gsc
 pause
