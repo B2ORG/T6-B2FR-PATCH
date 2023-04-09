caee1bd05db867d0a0be07f164469be0d9873c66
Zi0MIX <80110206+Zi0MIX@users.noreply.github.com>
2023-04-10 01:47:04 +0200

    Added main() removal to python preprocessor

--- release.py
+++ release.py
@@ -6,30 +6,39 @@ import os.path
 import re
 
 
-def replace_print_with_stub(cwd: str) -> None:
+def preprocessor(cwd: str) -> None:
+    filepath = os.path.join(cwd, "first_room_fix_v6_copy.gsc")
+    with open(filepath, "r", encoding="utf-8") as gsc_io:
+        gsc_content = gsc_io.readlines()
 
-    def get_new_line(lines: str, pattern: re.Pattern) -> str:
-        for i, line in enumerate(lines):
-            if re.search(pattern, line) is not None:
-                print(f"Replaced 'print' in line {i}")
-                yield line.replace("print", "stub")
-            else:
-                yield line
+    patterns = {
+        "print": {
+            "pattern": re.compile(r"\sprint\("),
+            "replacing": "print(",
+            "replacement": "stub(",
+        },
+        "main": {
+            "pattern": re.compile(r"^main\(\)\s[^}]*}\s", re.MULTILINE),
+            "replacing": None,
+            "replacement": "",
+        }
+    }
 
-    try:
-        pattern = re.compile("\sprint\(")
-        filepath = os.path.join(cwd, "first_room_fix_v6_copy.gsc")
-        with open(filepath, "r", encoding="utf-8") as gsc_io:
-            gsc_content = gsc_io.readlines()
+    new_gsc = "".join(gsc_content)
 
-        new_gsc = [l for l in get_new_line(gsc_content, pattern)]
+    for pattern_data in patterns.values():
+        while (match_obj := re.search(pattern_data["pattern"], new_gsc)) is not None:
+            gsc_before = new_gsc[:match_obj.start()]
+            gsc_after = new_gsc[match_obj.end():]
+            gsc_remainer = new_gsc[match_obj.start():match_obj.end()]
 
-        with open(filepath, "w", encoding="utf-8") as gsc_io:
-            gsc_io.writelines(new_gsc)
+            gsc_remainer = gsc_remainer.replace(pattern_data["replacing"], pattern_data["replacement"]) if pattern_data["replacing"] is not None else pattern_data["replacement"]
 
-    except Exception:
-        print_exc()
-        sys.exit()
+            new_gsc = f"{gsc_before}{gsc_remainer}{gsc_after}"
+            print(f"new_fragment={gsc_remainer} matched={match_obj}")
+
+    with open(filepath, "w", encoding="utf-8") as gsc_io:
+        gsc_io.write(new_gsc)
 
 
 def wrap_subprocess_call(*calls: str, **sbp_args) -> subprocess.CompletedProcess:
@@ -61,8 +70,12 @@ def main(cfg) -> None:
         wrap_subprocess_call("gsc-tool.exe", "comp", "t6", "first_room_fix_v6.gsc")
         # Copy main script
         wrap_subprocess_call("COPY", "/y", f"\"{os.path.join(cwd, 'first_room_fix_v6.gsc')}\"", f"\"{os.path.join(cwd, 'first_room_fix_v6_copy.gsc')}\"", shell=True)
-        # Replace ::print with ::stub in the copy
-        replace_print_with_stub(cwd)
+        # Preprocess the GSC content
+        try:
+            preprocessor(cwd)
+        except Exception:
+            print_exc()
+            sys.exit()
         # Compile copy
         wrap_subprocess_call("Compiler.exe", "first_room_fix_v6_copy.gsc")
         # Move compiled copy to the same folder as main
