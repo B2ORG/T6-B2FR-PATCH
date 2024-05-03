e04524d10227796b5e7988374941d71c76ae1d4a
Zi0MIX <80110206+Zi0MIX@users.noreply.github.com>
2024-05-03 14:51:22 +0200

    Compile script update

--- compile.py
+++ compile.py
@@ -1,26 +1,111 @@
 from traceback import print_exc
-import subprocess
-import sys
-import os
+import subprocess, sys, os, zipfile, re
+
+
+class Version:
+    UNKNOWN = [-1, -1, -1]
+    """Signature of an unknown version"""
+
+
+    def __init__(self) -> None:
+        self._version: list[int]
+
+
+    def __eq__(self, __object) -> bool:
+        return self._version == __object._version
+    
+
+    def __ne__(self, __object) -> bool:
+        return self._version != __object._version
+
+
+    def __lt__(self, __object) -> bool:
+        if self._version[0] < __object._version[0]:
+            return True
+        if self._version[0] <= __object._version[0] and self._version[1] < __object._version[1]:
+            return True
+        if self._version[0] <= __object._version[0] and self._version[1] <= __object._version[1] and self._version[2] < __object._version[2]:
+            return True
+        return False
+
+
+    def __gt__(self, __object) -> bool:
+        if self._version[0] > __object._version[0]:
+            return True
+        if self._version[0] >= __object._version[0] and self._version[1] > __object._version[1]:
+            return True
+        if self._version[0] >= __object._version[0] and self._version[1] >= __object._version[1] and self._version[2] > __object._version[2]:
+            return True
+        return False
+
+
+    def __le__(self, __object) -> bool:
+        return self == __object or self < __object
+
+
+    def __ge__(self, __object) -> bool:
+        return self == __object or self > __object
+
+
+    def __str__(self) -> str:
+        return ".".join([str(v) for v in self._version])
+
+
+    @staticmethod
+    def parse(_version: str):
+        version = Version()
+        version._version = [int(v) for v in _version.split(".")]
+        version.trim()
+        return version
+
+
+    def trim(self):
+        if len(self._version) > 3:
+            self._version = self._version[:3]
+        return self
+
+
+class UnknownVersion(Version):
+    def __init__(self) -> None:
+        super().__init__()
+        self._version = self.UNKNOWN
+
+
+class Chunk:
+    def __init__(self, header: str = None) -> None:
+        self.header = header
+
+
+    def __enter__(self):
+        if self.header is not None:
+            print(self.header)
+        print("-" * 100)
+
+
+    def __exit__(self, *args):
+        print("-" * 100, "\n")
 
 
 # Config
 CWD = os.path.dirname(os.path.abspath(__file__))
 B2FR = "b2fr.gsc"
-B2FR_COMPILED = "b2fr-compiled.gsc"
 B2FR_PARSED_NOHUD = "b2fr_nohud.gsc" 
 B2FR_PARSED_HUD = "b2fr_hud.gsc"
 GAME_PARSE = "iw5"      # Change later once it's actually implemented for t6
 GAME_COMP = "t6"
 MODE_PARSE = "parse"
 MODE_COMP = "comp"
-COMPILER_XENSIK = "gsc-tool.exe"
-PARSED_DIR = os.path.join("parsed", GAME_PARSE)
-COMPILED_DIR = os.path.join("compiled", GAME_COMP)
-PLUGIN_DIR = "plugin_templates"
+COMPILER_GSCTOOL = "gsc-tool.exe"
+PARSED_DIR = "parsed\\" + GAME_PARSE
+COMPILED_DIR = "compiled\\" + GAME_COMP
+ZMUTILITY_DIR = r"maps\mp\zombies"
 REPLACE_DEFAULT = {
-    "#define NOHUD 1": "#define NOHUD 0",
+    
 }
+PRECOMPILED = {
+    "pluto": "b2fr_precompiled_pluto.gsc",
+}
+BAD_COMPILER_VERSIONS = []
 
 
 def edit_in_place(path: str, **replace_pairs) -> None:
@@ -36,16 +121,18 @@ def edit_in_place(path: str, **replace_pairs) -> None:
         gsc_io.write(gsc_content)
 
 
-def wrap_subprocess_call(*calls: str, timeout: int = 5, **sbp_args) -> subprocess.CompletedProcess:
-    call = " ".join(calls)
+def wrap_subprocess_call(*calls: str, timeout: int = 5, cli_output: bool = True, **sbp_args) -> subprocess.CompletedProcess:
+    call: str = " ".join(calls)
     try:
         print(f"Call: {call}")
-        process = subprocess.run(call, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=timeout, **sbp_args)
+        process: subprocess.CompletedProcess = subprocess.run(call, capture_output=True, universal_newlines=True, timeout=timeout, **sbp_args)
     except Exception:
         print_exc()
-        sys.exit()
+        sys.exit(1)
     else:
-        print(process.stdout.decode())
+        print("Output:")
+        print(process.stdout + process.stderr if cli_output else "suppressed")
+        return process
 
 
 def arg_path(*paths: str) -> str:
@@ -59,26 +146,87 @@ def file_rename(old: str, new: str) -> None:
         os.rename(old, new)
 
 
-def main(cfg: list) -> None:
+# def create_zipfile() -> None:
+#     try:
+#         with zipfile.ZipFile(os.path.join(CWD, COMPILED_DIR, "b2op-ancient.zip"), "w", compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zip:
+#             zip.write(os.path.join(CWD, COMPILED_DIR, "b2op-ancient.gsc"), os.path.join(ZMUTILITY_DIR, "_zm_utility.gsc"))
+#     except FileNotFoundError:
+#         print("WARNING! Failed to create zip file due to missing compiled file")
+
+
+def verify_compiler() -> Version | bool:
+    return verify_compiler_version() if os.path.isfile(os.path.join(CWD, COMPILER_GSCTOOL)) else False
+
+
+def verify_compiler_version() -> Version:
+    compiler: subprocess.CompletedProcess = wrap_subprocess_call(COMPILER_GSCTOOL, cli_output=False)
+    lines: list[str] = compiler.stdout.split("\n")
+    if not lines:
+        print("Could not verify compiler version")
+        return UnknownVersion()
+
+    version: list[str] = re.compile(r"([\d.]+)").findall(lines[0])
+    if len(version) != 1:
+        print("Could not verify compiler version")
+        return UnknownVersion()
+
+    ver: Version = Version.parse(version[0])
+    if ver < Version.parse("1.4.0") or ver in BAD_COMPILER_VERSIONS:
+        input(f"WARNING! Potentially incompatibile version of the compiler ({str(ver)}) was found. Press ENTER to continue")
+    return ver
+
+
+def main() -> None:
     os.chdir(CWD)
+    print(f"\nSet CWD to: {os.getcwd()}\n")
+
+    # Util
+    compiler: Version | bool = verify_compiler()
+    if compiler is False:
+        print(f"'{COMPILER_GSCTOOL}' compiler executable not found in '{CWD}'")
+        sys.exit(1)
+    print()
 
     # NOHUD
-    nuhud_update = dict(REPLACE_DEFAULT)
-    nuhud_update.update({"#define NOHUD 0": "#define NOHUD 1"})
-    edit_in_place(os.path.join(CWD, B2FR), **nuhud_update)
-    wrap_subprocess_call(COMPILER_XENSIK, "-m", MODE_PARSE, "-g", GAME_PARSE, "-s", "pc", B2FR)
-    wrap_subprocess_call(COMPILER_XENSIK, "-m", MODE_COMP, "-g", GAME_COMP, "-s", "pc", arg_path(CWD, PARSED_DIR, B2FR))
-    file_rename(os.path.join(CWD, PARSED_DIR, B2FR), os.path.join(CWD, PARSED_DIR, B2FR_PARSED_NOHUD))
-    file_rename(os.path.join(CWD, COMPILED_DIR, B2FR), os.path.join(CWD, COMPILED_DIR, B2FR_PARSED_NOHUD))
+    with Chunk("B2FR - NOHUD:"):
+        nuhud_update = dict(REPLACE_DEFAULT)
+        nuhud_update.update({"#define NOHUD 0": "#define NOHUD 1"})
+        edit_in_place(
+            os.path.join(CWD, B2FR), **nuhud_update
+        )
+        wrap_subprocess_call(
+            COMPILER_GSCTOOL, "-m", MODE_PARSE, "-g", GAME_PARSE, "-s", "pc", B2FR
+        )
+        wrap_subprocess_call(
+            COMPILER_GSCTOOL, "-m", MODE_COMP, "-g", GAME_COMP, "-s", "pc", arg_path(CWD, PARSED_DIR, B2FR)
+        )
+        file_rename(
+            os.path.join(CWD, PARSED_DIR, B2FR), os.path.join(CWD, PARSED_DIR, B2FR_PARSED_NOHUD)
+        )
+        file_rename(
+            os.path.join(CWD, COMPILED_DIR, B2FR), os.path.join(CWD, COMPILED_DIR, B2FR_PARSED_NOHUD)
+        )
 
     # HUD
-    hud_update = dict(REPLACE_DEFAULT)
-    edit_in_place(os.path.join(CWD, B2FR), **hud_update)
-    wrap_subprocess_call(COMPILER_XENSIK, "-m", MODE_PARSE, "-g", GAME_PARSE, "-s", "pc", B2FR)
-    wrap_subprocess_call(COMPILER_XENSIK, "-m", MODE_COMP, "-g", GAME_COMP, "-s", "pc", arg_path(CWD, PARSED_DIR, B2FR))
-    file_rename(os.path.join(CWD, PARSED_DIR, B2FR), os.path.join(CWD, PARSED_DIR, B2FR_PARSED_HUD))
-    file_rename(os.path.join(CWD, COMPILED_DIR, B2FR), os.path.join(CWD, COMPILED_DIR, B2FR_PARSED_HUD))
+    with Chunk("B2FR - HUD:"):
+        hud_update = dict(REPLACE_DEFAULT)
+        hud_update.update({"#define NOHUD 1": "#define NOHUD 0"})
+        edit_in_place(
+            os.path.join(CWD, B2FR), **hud_update
+        )
+        wrap_subprocess_call(
+            COMPILER_GSCTOOL, "-m", MODE_PARSE, "-g", GAME_PARSE, "-s", "pc", B2FR
+        )
+        wrap_subprocess_call(
+            COMPILER_GSCTOOL, "-m", MODE_COMP, "-g", GAME_COMP, "-s", "pc", arg_path(CWD, PARSED_DIR, B2FR)
+        )
+        file_rename(
+            os.path.join(CWD, PARSED_DIR, B2FR), os.path.join(CWD, PARSED_DIR, B2FR_PARSED_HUD)
+        )
+        file_rename(
+            os.path.join(CWD, COMPILED_DIR, B2FR), os.path.join(CWD, COMPILED_DIR, B2FR_PARSED_HUD)
+        )
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
